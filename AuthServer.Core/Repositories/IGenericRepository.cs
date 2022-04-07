2e8feb7fb31e053f164255d4d62018277289ff6e
Samet UCA <sametuca@hotmail.com>
2022-04-07 21:46:32 +0300

    added error dto and generic repository interface

---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> Where(Expression<Func<TEntity,bool>> expression);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IQueryable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T : class 
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+        
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+        public ErrorDto Error { get; private set; }
+
+        public static Response<T> Success(T data,int statusCode=200)
+        {
+            return new Response<T> { Data = data, 
+                StatusCode = statusCode,IsSuccessful=true };
+        }
+
+        public static Response<T> Success(int statusCode=200)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, IsSuccessful = true };    
+        }
+
+        public static Response<T> Fail(ErrorDto erroDto,int statusCode)
+        {
+            return new Response<T> { Error = erroDto, StatusCode = statusCode, IsSuccessful = false };
+        }
+
+        public static Response<T> Fail(string errorMessage, int statusCode,bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccessful = false };
+        }
+
+
+    }
+}
