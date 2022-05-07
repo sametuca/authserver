d093f04903850e4170d90c1e404641a058258eb7
Samet UCA <sametuca@hotmail.com>
2022-05-07 20:50:33 +0300

    added client dto

---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<Response<TokenDto>> CreateTokenByRefreshTokenAsync(LoginDto loginDto);
+
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+
+    }
+}
---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using SharedLibrary.Dtos;
+using System.Threading.Tasks;
+using AuthServer.Core.DTOs;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto userDto);
+        Task<Response<UserAppDto>> GetUserByNameAsync(string userName);
+
+    }
+}
